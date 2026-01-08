6525f0ddae2b191ea732e2456f9ebbc82a4eacd6
SachinYadav1986 <sy8761250@gmail.com>
2026-01-08 15:23:33 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 10.2 MB (6.67%)

---
+++ 0020-valid-parentheses/solution.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool isValid(string s) {
+    stack<int> st;
+    for(auto c:s){
+        if((c=='(')|| (c=='[') || (c=='{')) st.push(c);
+        else{
+            if(st.empty()) return false;
+            if((st.top()=='(' && c==')') || (st.top()=='[' && c==']') || (st.top()=='{' && c=='}')) st.pop();
+            else{
+                return false;
+            }
+            }  
+        }
+    return st.empty();
+    }
+};
