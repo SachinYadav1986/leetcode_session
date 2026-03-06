b3c79d44fd919d40df13f524a9446add40be0117
SachinYadav1986 <sy8761250@gmail.com>
2026-03-06 13:11:36 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 9.6 MB (76.00%)

---
+++ 0242-valid-anagram/solution.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if(s.size()!=t.size()) return false;
+        int hash[26]={0};
+        for(int i=0;i<s.size();i++){
+            hash[s[i]-'a']++;
+        }
+
+        for(int i=0;i<t.size();i++){
+            if(hash[t[i]-'a']==0) return false;
+            hash[t[i]-'a']--;
+
+        }
+    return true;
+    }
+};
